434d7ec75bac9f5eb44f8fa5ee8f0315acee9ee1
pishivam009 <piyush.shivam2017@vitstudent.ac.in>
2021-06-22 12:33:11 +0530

    User, Doctor, Admin Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
